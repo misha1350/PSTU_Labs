7e4027596459ab12f48a9a3591502942a61ecd89
misha1350 <74230991+misha1350@users.noreply.github.com>
2021-02-13 14:16:18 +0500

    Вродё всё

--- №8/№8.cpp
+++ №8/№8.cpp
@@ -1,12 +1,3 @@
-//Всё работает, но код некрасивый, и лишние принты использовались для отладки. Также отсутствует защита от дурака
-
-
-
-
-
-
-
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -24,9 +15,9 @@ struct film_info {
 	string name, director, country;
 	float box_office_$M;
 
-	void Print() //Починить вывод значений переменных
+	void Print()
 	{
-		cout << "Название:\t" << name << "\nРежиссёр:\t" << director << "\nСтрана:\t" << country << "\nКассовые Сборы : \t" << box_office_$M << endl;
+		cout << "\nНазвание:\t" << name << "\nРежиссёр:\t" << director << "\nСтрана:\t\t" << country << "\nКассовые Сборы:\t" << box_office_$M << endl;
 	}
 };
 
@@ -36,7 +27,7 @@ void movie(film_info*& film, int N)
 	for (int i = 0; i < N; i++)
 	{
 		cin.ignore();
-		cout << endl << "Введите название фильма №" << i + 1 << ": ";
+		cout << endl << "\nВведите название фильма №" << i + 1 << ": ";
 		getline(cin, film[i].name);
 		cout << "\nВведите ФИ режиссёра: ";
 		getline(cin, film[i].director);
@@ -45,20 +36,6 @@ void movie(film_info*& film, int N)
 		cout << endl << "Введите общие кассовые сборы в миллионах долларов: ";
 		cin >> film[i].box_office_$M;
 	}
-}
-
-void movie_add(film_info*& film_add, int nadd)
-{
-	setlocale(LC_ALL, "ru");
-		cin.ignore();
-		cout << endl << "Введите название фильма: ";
-		getline(cin, film_add[nadd].name);
-		cout << endl << "Введите ФИ режиссёра: ";
-		getline(cin, film_add[nadd].director);
-		cout << endl << "Введите страну: ";
-		getline(cin, film_add[nadd].country);
-		cout << endl << "Введите общие кассовые сборы в миллионах долларов: ";
-		cin >> film_add[nadd].box_office_$M;
 }
 
 void erase(film_info*& film, film_info*& film_erase, int& N)
@@ -70,9 +47,9 @@ void erase(film_info*& film, film_info*& film_erase, int& N)
 		film_erase[i].country = film[i].country;
 		film_erase[i].box_office_$M = film[i].box_office_$M;
 	}
+	cout << "\n\n" << "Удалены два фильма в конце списка. Новый список:\n";
 	for (int i = 0; i < N - 2; i++)
 	{
-		cout << "\n\n";
 		film_erase[i].Print();
 		cout << "\n";
 	}
@@ -80,9 +57,9 @@ void erase(film_info*& film, film_info*& film_erase, int& N)
 	Na = N - 2;
 }
 
-void add(film_info*& film_erase, film_info*& film_add, film_info*& film, int& N, int& Na) // Выходит за рамки массива
+void add(film_info*& film_erase, film_info*& film_add, int& N, int& Na)
 {
-	int target_number = 0;
+	bool target_number = 0;
 	string target_name;
 	cout << "\n\nВведите название фильма, после которого надо добавить элемент: ";
 	cin >> target_name;
@@ -90,7 +67,7 @@ void add(film_info*& film_erase, film_info*& film_add, film_info*& film, int& N,
 	{
 		if (film_erase[i].name == target_name)
 		{
-			target_number = i;
+			target_number = true;
 			for (int j = 0; j <= i; j++) // В новый третий массив записываются все значения до нового добавленного элемента
 			{
 				film_add[j].name = film_erase[j].name;
@@ -98,50 +75,35 @@ void add(film_info*& film_erase, film_info*& film_add, film_info*& film, int& N,
 				film_add[j].country = film_erase[j].country;
 				film_add[j].box_office_$M = film_erase[j].box_office_$M;
 			}
-			for (int k = 0; k < Na + 1; k++)
-			{
-				cout << "\n\n";
-				film_add[k].Print();
-				cout << "\n";
-			}
 			cout << "\n\nВведите данные нового фильма.";
-			int nadd = target_number + 1;
-			movie_add(film, nadd + 1);
-			
-			for (int k = 0; k < Na + 1; k++)
-			{
-				cout << "\n\n";
-				cout << i;
-				film_add[k].Print();
-				cout << "\n";
-			}
+			cin.ignore();
+			cout << endl << "Введите название фильма №" << i + 2 << ": ";
+			getline(cin, film_add[i+1].name);
+			cout << "\nВведите ФИ режиссёра: ";
+			getline(cin, film_add[i+1].director);
+			cout << "\nВведите страну: ";
+			getline(cin, film_add[i+1].country);
+			cout << endl << "Введите общие кассовые сборы в миллионах долларов: ";
+			cin >> film_add[i+1].box_office_$M;
 			for (int l = i + 2; l < Na + 1; l++)
 			{
-				film_add[l].name = film_erase[l].name;
-				film_add[l].director = film_erase[l].director;
-				film_add[l].country = film_erase[l].country;
-				film_add[l].box_office_$M = film_erase[l].box_office_$M;
-			}
-			i = N;
-			for (int k = 0; k < Na + 1; k++)
-			{
-				cout << i << "\n";
-				cout << "\n\n";
-				film_add[k].Print();
-				cout << "\n";
+				film_add[l].name = film_erase[l - 1].name;
+				film_add[l].director = film_erase[l - 1].director;
+				film_add[l].country = film_erase[l - 1].country;
+				film_add[l].box_office_$M = film_erase[l - 1].box_office_$M;
 			}
 		}
 	}
 	if (target_number == 0)
 	{
-		cout << "Название не найдено.\n";
+		cout << "\nНазвание не найдено.\n";
 	}
 }
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	int N;
+	int N, target_number{};
 	cout << "Введите количество элементов (фильмов): ";
 	cin >> N;
 	int Na = N - 2;
@@ -151,12 +113,14 @@ int main()
 	film_info* film_add = new film_info[Na + 1];
 	movie(film, N);
 	erase(film, film_erase, N);
-	if (N != 0)
+	delete[] film;
+	add(film_erase, film_add, N, Na);
+
+	if (film_add[0].name != "")
 	{
-		add(film_erase, film_add, film, N, Na);
+		cout << "\n\nСписок с добавленным элементом:\n";
 		for (int i = 0; i < Na + 1; i++)
 		{
-			cout << "\n\n";
 			film_add[i].Print();
 			cout << "\n";
 		}
@@ -165,5 +129,7 @@ int main()
 	{
 		cout << "\nДобавить невозможно!\n";
 	}
+	delete[] film_erase;
+	delete[] film_add;
 	return 0;
 }
