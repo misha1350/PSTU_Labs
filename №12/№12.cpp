747a6b0aee06a6f98e7a7d6d9851abf2e1c723d6
misha1350 <74230991+misha1350@users.noreply.github.com>
2021-02-26 13:41:26 +0500

    Update №12.cpp

--- №12/№12.cpp
+++ №12/№12.cpp
@@ -1,4 +1,4 @@
-//WIP
+// WIP
 
 #include <iostream>
 #include <string>
@@ -12,7 +12,7 @@ struct str
 {
   string name;
   string date_of_birth;
-  string passport;
+  string passport_region;
   int pos;
 };
 
@@ -20,8 +20,8 @@ void fill_out(vector<str> &citizen, int n)
 {
   citizen.clear();
   ifstream name1("name.txt");
-  string name2[100];
-  for (int i = 0; i < 100; i++)
+  string name2[101];
+  for (int i = 0; i < 101; i++)
   {
     string data;
     name1 >> data;
@@ -31,7 +31,7 @@ void fill_out(vector<str> &citizen, int n)
   str test;
   for (int i = 0; i < n; i++)
   {
-    test.name = name2[rand() % n+1];
+    test.name = name2[rand() % (35)];
     test.pos = i + 1;
     citizen.push_back(test);
   }
@@ -42,7 +42,48 @@ void print(vector<str> citizen, int n)
   for (int i = 0; i < n; i++)
   {
     cout << citizen[i].pos << endl;
-    cout << citizen[i].name << endl << endl;
+    cout << citizen[i].name << "\n\n";
+  }
+}
+
+void save(vector<str> citizen, int n)
+{
+  string save_name;
+  cout << "Введите название файла для сохранения...\t";
+  cin >> save_name; 
+  save_name += ".txt";
+  ofstream save(save_name);
+  for (int i = 0; i < n; i++)
+  {
+    save << citizen[i].pos << endl;
+    save << citizen[i].name << "\n\n";
+  }
+  save.close();
+  cout << "\nФайл сохранён.";
+}
+
+void search(string str2, string sub, bool flag)
+{
+  int str_line, sub_line;
+  str_line = str2.size();
+  sub_line = sub.size();
+  if (sub_line != 0 && str_line != 0)
+  {
+    for (int i = 0; i <str_line - sub_line + 1; i++)
+    {
+      for (int j = 0; j < sub_line; j++)
+      {
+        if (sub [j] != str2[i+1])
+        {
+          goto retard;
+        }
+        else if (j == sub_line - 1)
+        {
+          flag = true;
+          goto retard;
+        }
+      }
+    }
   }
 }
 
@@ -58,6 +99,9 @@ int main()
   fill_out(citizen, n);
   print(citizen, n);
   
+  save(citizen, n);
+
+
   return 0;
 }
 
