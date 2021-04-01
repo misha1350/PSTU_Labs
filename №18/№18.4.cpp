6c72870f7ce4ec55025e273e2c5e5facfecd19c1
misha1350 <74230991+misha1350@users.noreply.github.com>
2021-04-01 16:08:24 +0500

    Create №18.4.cpp

---
+++ №18/№18.4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+#include "triad.h"
+#include "time.h"
+
+int main() {
+  Time a(2020, 10, 25);
+  cout << a;
+
+  Time b(2021, 3, 21);
+  cout << b;
+}
