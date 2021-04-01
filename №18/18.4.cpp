e66f359563bf3568bfe5139d93a0f823ac705aed
misha1350 <74230991+misha1350@users.noreply.github.com>
2021-04-01 23:19:37 +0500

    Update 18.4.cpp

--- №18/18.4.cpp
+++ №18/18.4.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include "triad.h"
-#include "time.h"
 
 using namespace std;
+
+#include "triad.h"
+#include "time.h"
 
 int main() {
   Time a(2020, 10, 25);
@@ -10,4 +11,12 @@ int main() {
 
   Time b(2021, 3, 21);
   cout << b;
+
+  bool comparison = a == b;
+  if (comparison == true) cout << "\nA == B";
+  else cout << "\nA != B";
+
+  bool greater_than = a > b;
+  if (greater_than == true) cout << "\nA > B";
+  else cout << "\nA < B";
 }
