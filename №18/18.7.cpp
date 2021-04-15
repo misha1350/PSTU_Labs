644beb755b5cd8aa03cc2d9da285a4cf129ccfb2
misha1350 <74230991+misha1350@users.noreply.github.com>
2021-04-15 22:45:47 +0500

    Update 18.7.cpp

--- №18/18.7.cpp
+++ №18/18.7.cpp
@@ -5,23 +5,40 @@
 [] - доступа по индексу
 != - проверка на неравенство
 < число - принадлежность числа множеству.
-
 Пользовательский класс Money для работы с денежными суммами. Число должно быть представлено двумя полями: типа long для рублей и типа int для копеек. Дробная часть числа при выводе на экран  должна быть отделена от целой запятой.
 */
 
 #include <iostream>
-#include "Headers 6-13/array2.h"
-#include "Headers 6-13/money2.h"
+#include "Headers 6-13/array7.h"
+#include "Headers 6-13/money7.h"
 
 using namespace std;
 
 int main() {
   setlocale (LC_ALL, "ru");
-  array<int> arr (5, 0);
+
+  money t;
+  cin >> t;
+  cout << t << endl;
+
+  int size1, size2;
+  cout << "size1? "; cin >> size1;
+  cout << "size2? "; cin >> size2;
+
+  Array<money> arr(size1, t);
   cin >> arr;
   cout << arr << endl;
 
-  // array<int> d (10, 0);
-  // cin >> d;
-  // cout << d << endl;
+  Array<money> d(size2, t);
+  cout << d << endl;
+  cin >> d;
+  cout << d << endl;
+  d = arr;
+  cout << d << endl;
+
+  cout << arr[2] << endl;
+  cout << d[7] << endl; //arr[7]
+
+
+  return 0;
 }
