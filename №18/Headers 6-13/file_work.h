ef8c36ebfff26b86275ea325fbf2c0e42e941358
misha1350 <74230991+misha1350@users.noreply.github.com>
2021-04-24 18:31:51 +0500

    Update file_work.h

--- №18/Headers 6-13/file_work.h
+++ №18/Headers 6-13/file_work.h
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-int make_file (const char* f_name)
-{
+int make_file (const char* f_name) {
   fstream stream (f_name, ios::out|ios::trunc);
   if(!stream)return -1;
   int n;
@@ -20,8 +19,7 @@ int make_file (const char* f_name)
   return n;
 }
 
-int print_file (const char *f_name)
-{
+int print_file (const char *f_name) {
   fstream stream(f_name, ios::in);
   if (!stream) return -1;
   money t; int i = 0;
@@ -31,4 +29,68 @@ int print_file (const char *f_name)
   }
   stream.close();
   return i;
+}
+
+int add_entry(const char *f_name, int num, money t1) {
+  fstream temp("temp", ios::out);
+  fstream stream(f_name, ios::in);
+  if (!stream) return -1;
+  money t; int i = 0, l = 0;
+  while (stream >> t) {
+    if (stream.eof()) break;
+    i++;
+    if(i == num) {
+      temp << t1;
+      l++;
+    }
+    temp << t;
+  }
+  stream.close(); temp.close();
+  remove(f_name);
+  rename("temp", f_name);
+  return l;
+}
+
+int add_end(const char *f_name, money t1) {
+  fstream stream(f_name, ios::app);
+  if(!stream) return -1;
+  stream << t1;
+  return 1;
+}
+
+int del_entry (const char *f_name, int num) {
+  fstream temp("temp", ios::out);
+  fstream stream (f_name, ios::in);
+  if (!stream) return -1;
+  money t; int i = 0;
+  while (stream >> t) {
+    if (stream.eof()) break;
+    i++;
+    if (i!=num) temp << t;
+  }
+  stream.close(); temp.close();
+  remove(f_name);
+  rename("temp", f_name);
+  return i;
+}
+
+int change_entry(const char *f_name, int num, money t2) {
+  fstream temp("temp", ios::out);
+  fstream stream(f_name, ios::in);
+  if (!stream) return -1;
+  money t; int i = 0, l = 0;
+  while (stream >> t) {
+    if (stream.eof()) break;
+    i++;
+    if (i == num) {
+      cout << endl << t << " is being replaced\n";
+      temp << t2;
+      l++;
+    }
+    else temp << t;
+  }
+  stream.close(); temp.close();
+  remove(f_name);
+  rename("temp", f_name);
+  return l;
 }
